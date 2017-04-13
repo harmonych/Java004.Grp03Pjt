0b6cffc57756173f029ac685ceacaf78bf4179db
Harmony Chang <duke.harmony@gmail.com>
2017-04-13 19:55:43 +0800

    此js用途為提供動態生成頁面內與網頁互動的方式，只要你的內容是利用動態生成，遵循條件便能套用這code，換頁換成創作者、募資作品與商品其中一者的詳細頁面。 觸發條件：網頁內容上該段tag有class = "img-link" id為"ip_xx_idy" xx{fc || pr || ar} y為數字。 便能轉頁至id為y的詳細頁面。

--- WiseProject/WebContent/js/default.js
+++ WiseProject/WebContent/js/default.js
@@ -1,30 +1,37 @@
 $(document).ready(function () {
-	 /*置換內頁 */
+	/*置換內頁 */
+	/*至指定創作者頁面*/
 	$("#sp_artist_id").click(function(){
 		$.ajax({
 			url:"\_05_CreationsFrame\\CreationsPage2.jsp",
 			context: document.body,
-			success: function(response){
-				$('#mainframe').html(response);
+			success: function(responseSpArtId){
+				$("#mainframe").empty();
+				$('#mainframe').html(responseSpArtId);
 				}
   	    });
   	  })
   	  
+	/*至指定商品頁面*/
   	  $("#sp_pro_id").click(function(){
   		  $.ajax({
   			  url:"\_05_CreationsFrame\\CreationsPage3.jsp",
   			  context: document.body,
-  			  success: function(response){
-  				  $('#mainframe').html(response);
+  			  success: function(responseSpProId){
+  				  $("#mainframe").empty();
+  				  $('#mainframe').html(responseSpProId);
   			  }
   		  });
   	  });
+
+	/*至指定募資作品頁面*/
 	  $("#sp_fc_details").click(function(){
   		  $.ajax({
   			  url:"\_11_Fc_info\\Fc_Info.jsp",
   			  context: document.body,
-  			  success: function(response){
-  				  $('#mainframe').html(response);
+  			  success: function(responseSpFcId){
+  				  $("#mainframe").empty();
+  				  $('#mainframe').html(responseSpFcId);
   			  }
   		  });
   	  });
@@ -34,10 +41,86 @@ $(document).ready(function () {
   			  url:"\_11_Fc_info\\Fc_Info2.jsp",
   			  context: document.body,
   			  success: function(response){
-  				  $('#mainframe').html(response);
+  				$("#mainframe").empty();
+  				$('#mainframe').html(response);
   			  }
   		  });
   	  });
+	/*動態置換內頁，若不懂為何要特意獨立這寫法出來請問我@Harmony*/
+	  $(document).on('click', ".img-link" , function(){
+		  //抓出內頁ID
+		  var uni_ip_id = $(this).attr('id');
+		  //命名邏輯為ip_xxx_id_y, xxx { fc, pro, art}, y為整數
+		  //start of first if-else
+		  if (uni_ip_id.substring(0,2)==="ip"){
+			  //先判斷是否為fc_id
+			  //start of second if-else
+			  if(uni_ip_id.substring(3,5)==="fc"){
+				  var fc_id = parseInt(uni_ip_id.replace( /[^\d.]/g, '' ), 10);
+				  console.log("this is fc_id" + uni_ip_id);
+		  		  $.ajax({
+		  			  //執行Servlet 07 DisplaySingleFundPage.java
+		  			  url:"\_11_Fc_info\\DisplayFund?fc_id=" + fc_id,
+		  			  context: document.body,
+		  			  type: "get",
+		  			  success: function(responseIpFcId){
+		  				  $("#mainframe").empty();
+		  				  $('#mainframe').html(responseIpFcId);
+		  			  },
+		  			  error: function(responseIpFcIdErr){
+		  				  console.log(fc_id);
+		  			  }
+		  		  });
+			  }else{
+				  //再判斷是否為pro_id
+				  //start of third if-else
+				  if(uni_ip_id.substring(3,5)==="pr"){
+					  var pro_id = parseInt(uni_ip_id.replace( /[^\d.]/g, '' ), 10);
+					  console.log("this is pro_id : " + uni_ip_id);
+			  		  $.ajax({
+			  			  //執行Servlet 08 DisplaySingleProductPage.java
+			  			  url:"\_13_Product_info\\DisplayProduct?pro_id=" + pro_id,
+			  			  context: document.body,
+			  			  type: "get",
+			  			  success: function(responseIpProId){
+			  				  $("#mainframe").empty();
+			  				  $('#mainframe').html(responseIpProId);
+			  			  },
+			  			  error: function(responseIpProIdErr){
+			  				  console.log(pro_id);
+			  			  }
+			  		  });
+				  }else{
+					  //最後判斷是否為art_id
+					  //start of fourth if-else
+					  if(uni_ip_id.substring(3,5)==="ar"){
+						  var art_id = parseInt(uni_ip_id.replace( /[^\d.]/g, '' ), 10);
+						  console.log("this is art_id : " + uni_ip_id);
+				  		  $.ajax({
+				  			  //執行Servlet 01 DisplaySingleProductPage.java
+				  			  url:"\_05_CreationsFrame\\DisplayArtist?art_id=" + art_id,
+				  			  context: document.body,
+				  			  type: "get",
+				  			  success: function(responseIpArtId){
+				  				  $("#mainframe").empty();
+				  				  $('#mainframe').html(responseIpArtId);
+				  			  },
+				  			  error: function(responseIpArtIdErr){
+				  				  console.log(art_id);
+				  			  }
+				  		  });
+					  }else{
+						  //三者皆非的狀況
+						  console.log("!!!!!!!!!!!!!!!!!!!!")
+						  console.log("無法辦別該uni_ip_id字串。")  
+						  console.log(uni_ip_id)
+					  }//end of fourth if-else
+				  }//end of third if-else
+				}//end of second if-else 
+			  }//end of first if-else
+		  
+  	  });
+	  /*動態置換內頁結束*/
     /* 置換內頁結束 */
    
 
